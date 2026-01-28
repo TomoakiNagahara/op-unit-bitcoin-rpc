01bfc88e2a41cff8040435696a8958a471cecda5
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-01-28 14:25:03 +0900

    New: instantiate.php

---
+++ instantiate.php
@@ -0,0 +1,24 @@
+<?php
+/**	op-unit-bitcoin-rpc:/instantiate.php
+ *
+ * @created    2026-01-28
+ * @license    Apache-2.0
+ * @package    op-unit-bitcoin-rpc
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+/**	Return the instance of Bitcoin RPC.
+ *
+ * @created    2026-01-28
+ */
+return new \OP\UNIT\BITCOIN\RPC();
